18d3f6d83bb3c48b88a3ae2dadc6ba3121b6d43c
Troy Unverdruss <troy@unverdruss.net>
2020-09-07 20:19:30 -0700

    connect to wifi and send status

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,56 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include "secrets.h"
 
 #define ONBOARD_LED 2
 #define SENSOR_PIN 23
 volatile byte state = LOW;
-int lastState = 0;
 
 __unused void setup() {
+    // Setup serial connection
     Serial.begin(115200);
+
+    // Setup pins
     pinMode(ONBOARD_LED, OUTPUT);
     pinMode(SENSOR_PIN, INPUT);
+
+    // Setup wifi
+    WiFi.begin(wifiNetworkName, wifiNetworkPassword);
+
+    long start = millis();
+    while (WiFiClass::status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+        delay(10);
+        Serial.print('.');
+    }
+    Serial.print("Connected to wifi after: ");
+    Serial.print(millis() - start);
+    Serial.println(" millis");
+
+    Serial.println('\n');
+    Serial.println("Connection established!");
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());
+}
+
+void sendHttp(volatile byte state) {
+    try {
+        HTTPClient http;
+        http.begin("http://10.41.33.50:8080/");
+        char buf[50];
+        int responseCode = http.POST(ltoa(state, buf, 10));
+        Serial.print("Response code: ");
+        Serial.println(responseCode);
+
+        while (http.connected()) {
+            String body = http.getString();
+            Serial.println(body);
+        }
+
+        http.end();
+    } catch (char const* s) {
+        Serial.println(s);
+    }
 }
 
 __unused void loop() {
@@ -22,4 +64,9 @@ __unused void loop() {
 
     if (state == LOW) digitalWrite(ONBOARD_LED, HIGH);
     else digitalWrite(ONBOARD_LED, LOW);
+
+    sendHttp(state);
+    delay(1000);
 }
+
+
---
+++ src/secrets.h
@@ -0,0 +1,11 @@
+//
+// Created by troy on 9/7/20.
+//
+
+#ifndef GARAGE_DOOR_SENSOR_SECRETS_H
+#define GARAGE_DOOR_SENSOR_SECRETS_H
+
+extern const char* wifiNetworkName;
+extern const char* wifiNetworkPassword;
+
+#endif //GARAGE_DOOR_SENSOR_SECRETS_H
